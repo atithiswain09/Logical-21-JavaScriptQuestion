7212671ee7fc127c027c4681386a06908fb69533
atithiswain <atithiswain371@gmail.com>
2025-05-03 23:42:30 +0530

    task-1 question

---
+++ Question1/script.js
@@ -0,0 +1,18 @@
+// Task: Write a program to determine if the top step of a staircase with n steps is even or odd.
+
+// Input: n (an integer representing the number of steps)
+
+// Output: "Even" or "Odd"
+
+// Example:
+
+// Input: n = 5
+
+// Output: "Odd"
+
+let num=prompt("Enter Your Number");
+if(num%2==0){
+    alert("This Is a Even number:-"+num);
+}else{
+    alert("This is a Odd number:-"+num);
+}
