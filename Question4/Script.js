278fb51c70d3de3f9137c73934baabb33c44875c
atithiswain <atithiswain371@gmail.com>
2025-06-28 23:30:20 +0530

    Question4

---
+++ Question4/Script.js
@@ -0,0 +1,18 @@
+// Can you determine if someone is eligible to vote based on their age? This challenge will help you get comfortable with using IF-ELSE statements.
+
+// Explanation
+// In this challenge, you'll use an IF-ELSE statement to check if a person meets the minimum age requirement for voting. You can set this minimum age based on your country’s laws.
+
+// Input - age (an integer representing the person's age) and min_age (an integer representing the minimum voting age)
+
+
+
+let age=prompt("Enter Your Age..");
+
+if(age<18){
+    alert("The Persion is Not Eligible for Vote")
+    
+}else{
+       alert("The Persion is  Eligible for Vote"
+);
+}
