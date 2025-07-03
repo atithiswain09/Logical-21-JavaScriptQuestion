8df0310593d9d106370b969e2f28ae22283bb820
atithiswain <atithiswain371@gmail.com>
2025-07-03 20:02:15 +0530

    Question5

---
+++ Question5/script.js
@@ -0,0 +1,10 @@
+let age =prompt("Please enter your age:");
+
+let min_drive_age=18;// am Declaring the Commen age of a persion who is Able to get a Driving Licence
+ 
+if(age==min_drive_age){
+      alert("Person is eligible ✅");
+}
+else{
+    alert("Person is not eligible ❌");
+}
